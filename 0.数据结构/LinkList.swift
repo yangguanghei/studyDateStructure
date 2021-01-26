de84bfe466c042b160e620bf73486ad9693047c6
liangsen2 <liangsende1@dingtalk.com>
2021-01-26 22:39:37 +0800

    单向链表

---
+++ 0.数据结构/LinkList.swift
@@ -0,0 +1,115 @@
+//
+//  LinkList.swift
+//  0.数据结构
+//
+//  Created by 梁森 on 2021/1/25.
+//
+
+import Foundation
+
+/// 节点类
+fileprivate class Node {
+    var item: String?
+    var next: Node?
+    init(item: String?, next: Node?) {
+        self.item = item
+        self.next = next
+    }
+}
+
+
+/// 链表
+class LinkList {
+       
+    /// 头节点
+    private var head = Node(item: nil, next: nil)
+    /// 链表长度
+    var length = 0
+    
+    /// 初始化链表
+    init() {
+        self.head = Node(item: nil, next: nil)
+        length = 0
+    }
+    
+    /// 添加元素
+    func addNode(item: String) {
+        var n = head
+        while n.next != nil {
+            n = n.next!
+        }
+        let newNode = Node(item: item, next: nil)
+        n.next = newNode
+        length += 1
+    }
+    
+    /// 插入元素
+    func insertNode(index: Int, item: String) {
+        var n = head
+        for _ in 0..<index {
+            n = n.next!
+        }
+        let currentN = n.next
+        let newNode = Node(item: item, next: nil)
+        n.next = newNode
+        newNode.next = currentN
+        length += 1
+    }
+    
+    /// 删除元素
+    func delete(index: Int) {
+        var n = head
+        for _ in 0..<index {
+            n = n.next!
+        }
+        let currentN = n.next
+        let nextN = currentN?.next
+        n.next = nextN
+        length -= 1
+    }
+    
+    /// 获取某个元素在链表的序号
+    func indexOf(item: String) -> Int {
+        let n = head
+        for i in 0..<length {
+            if n.item == item {
+                return i
+            }
+        }
+        return -1
+    }
+    
+    /// 清空
+    func clear() {
+        head.next = nil
+        length = 0
+    }
+    
+    /// 判断是否为空
+    func isEmpty() -> Bool {
+        if length == 0 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    /// 获取链表长度
+    func linkLength() -> Int {
+        return length
+    }
+    
+    /// 获取第i个元素的值
+    func getItem(index: Int) -> String? {
+        if index > length-1 || index < 0 {
+            return "越界了"
+        }
+        var node = head.next
+        for _ in 0..<index {
+            node = node?.next
+        }
+        return node?.item
+    }
+ 
+}
+
--- 0.数据结构/ViewController.swift
+++ 0.数据结构/ViewController.swift
@@ -12,8 +12,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .green
     }
 
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        testLink()
+    }
+}
 
+extension ViewController {
+    
+    /// 测试链表
+    private func testLink() {
+        let linkList = LinkList()
+        linkList.addNode(item: "1")
+        linkList.addNode(item: "2")
+        linkList.insertNode(index: 1, item: "3")
+        linkList.delete(index: 0)
+        linkList.clear()
+        
+        for i in 0..<linkList.length {
+            let text = linkList.getItem(index: i)
+            print(text!)
+        }
+    }
+    
 }
 
